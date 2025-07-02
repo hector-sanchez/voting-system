609c0c7c63ed238fd9d50f699248a56766e7e21e
Hector Sanchez <sanchez.hector.m@gmail.com>
2025-07-02 00:51:40 -0400

    Adds some tooling needed for testing and authentication

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem "redis", ">= 4.0.1"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
@@ -42,6 +42,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
   gem 'rspec-rails', '~> 6.1.0'
+  gem 'factory_bot_rails'
   gem 'faker'
   gem 'pry-byebug'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_02_043910) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "zipcode", null: false
+    t.string "password_digest", null: false
+    t.integer "token_version", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email", "zipcode"], name: "index_users_on_email_and_zipcode", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
